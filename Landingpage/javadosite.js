967d9441bdf2b40ee76a3b895ebb7d7841e412e4
Lucas Lima dos Santos <131780435+DesLucas98@users.noreply.github.com>
2023-11-29 19:47:56 -0300

    Landing Page
    Uma landing page no modelo da pagina DNC

---
+++ Landingpage/javadosite.js
@@ -0,0 +1,18 @@
+var leonardo = window.document.getElementById("leonardo")
+var bruna = window.document.getElementById("bruna")
+var samantha = window.document.getElementById("samantha")
+
+function Rolardireita() {
+    bruna.style="display:none"
+    samantha.style="display:flex"
+    setaesquerda.style="display:flex"
+    setadireita.style="display:none"
+
+}
+function Rolaresquerda() {
+    bruna.style="display:flex"
+    samantha.style="display:none"
+    setaesquerda.style="display:none"
+    setadireita.style="display:flex"
+
+}
